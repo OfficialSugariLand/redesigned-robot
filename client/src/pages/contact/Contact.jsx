4e118fc62e1fee551d3996fc280826f31b593ce0
Sugar iLand <officialsugariland.com>
2023-06-06 14:02:20 -0700

    commit

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -9,34 +9,30 @@ import { IoLogoWhatsapp } from "react-icons/io";
 
 export default function Contact() {
   return (
-    <div className="contactUs">
+    <div className="contact">
       <Topbar />
       <Header title="Contact us" image={SugarImg}>
-        Sugar iLand relies on your contributions to keep growing and it is her duty to listen to all users around the globe,
+        Sugar iLand relies on your contributions to keep growing, therefor it is her duty to listen to all users around the globe,
         if there's any issue encountered please use of the methods below to reach out to technical department to fix the issue
         in order to keep serving you a more user friendly application, in a single click below your report will be heard.
       </Header>
-      <section className="contact">
-        <div className="contact_container">
-          <div className="contact_wrapper">
-            <a href="mailto:sugariland@gmail.com" target="_blank" rel="noreferrer noopener">
-              <div className="messengerGmail">
-                <MdEmail />
-              </div>
-            </a>
-            <a href="http://m.me/100079946314990" target="_blank" rel="noreferrer noopener">
-              <div className="messengerFB">
-                <BsMessenger />
-              </div>
-            </a>
-            <a href="http://wa.me/+2349166494702" target="_blank" rel="noreferrer noopener">
-              <div className="messengerWhatsapp">
-                <IoLogoWhatsapp />
-              </div>
-            </a>
+      <div className="contact_US">
+        <a href="mailto:sugariland@gmail.com" target="_blank" rel="noreferrer noopener">
+          <div className="messengerGmail">
+            <MdEmail />
           </div>
-        </div>
-      </section>
+        </a>
+        <a href="http://m.me/100079946314990" target="_blank" rel="noreferrer noopener">
+          <div className="messengerFB">
+            <BsMessenger />
+          </div>
+        </a>
+        <a href="http://wa.me/+2349166494702" target="_blank" rel="noreferrer noopener">
+          <div className="messengerWhatsapp">
+            <IoLogoWhatsapp />
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
